8dc45082e4a0e093c669f3d3c99d2483cda1d650
yujingyou <yujingyou@flyaudio.cn>
2019-10-10 10:52:07 +0800

    上次提交漏掉的更新

--- filenameauto/urls.py
+++ filenameauto/urls.py
@@ -13,12 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
+from django.views.static import serve
+
+from filenameauto import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('nameauto/', include('name_generate.urls')),
+
+    url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
     path('', RedirectView.as_view(pattern_name='NameGenerate'), name='home'),
 ]
